202be93e5ec71383502ce80ec450da18139a845f
Chinonon <115488987+Chinonon@users.noreply.github.com>
2022-11-17 14:55:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+(function () {
+  const pictures = ["ba2","ba3","ba4","ba7","ba8","ba9"];
+
+ const buttons = document.querySelectorAll(".btn");
+ const imgDiv = document.querySelector(".image-container");
+
+ let counter = 0
+ 
+ buttons.forEach(function(button){
+    button.addEventListener("click", function(e){
+        if (button.classList.contains("btn-left")){
+            counter--
+        if (counter < 0){
+            counter = pictures.length - 1
+        }
+            imgDiv.style.background =`url("image-slide-img/${pictures[counter]}.webp")`
+        }
+
+        if (button.classList.contains("btn-right")){
+            counter++
+        if (counter > pictures.length - 1){
+            counter = 0
+        }
+            imgDiv.style.background =`url("image-slide-img/${pictures[counter]}.webp")`
+        }
+    })
+ })
+
+})();
